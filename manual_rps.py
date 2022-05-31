6c3d0b7fbf2cb5c6253298d9561dcba80d1061f4
a-AAKASH <aakashpaudyal@gmail.com>
2022-05-31 10:42:32 +0100

    Milestone 3: Creating a manual RPS game

---
+++ manual_rps.py
@@ -0,0 +1,42 @@
+## Creating the Rock, Paper and Scissors game with two functions 
+## one to get the computer's choice and the other for the user to input 
+
+import random 
+
+def get_computer_choice():
+    list = ['Rock', 'Paper', 'Scissors']
+    return random.choice(list)
+
+def get_user_choice():
+    choice = input("Enter your choice \(Rock, Paper or Scissors\): ")
+    return choice
+
+
+
+def get_winner(computer_choice, user_choice):
+    ## A dictionary that denotes the hierarchy of the options..eg. beats['Scissors'] will equal 'Rock',
+    ## as Rock beats scissors
+    beats = {
+    'Scissors': 'Rock',
+    'Rock': 'Paper',
+    'Paper': 'Scissors',
+    }
+    
+    ## Ref. for the logic in the if-elif-else loop from
+    ## https://codereview.stackexchange.com/questions/43024/use-if-else-elif-conditionals-to-write-a-basic-rock-paper-scissors-game#comment74280_43024
+    if computer_choice == user_choice:
+        print("Draw!! \n Try Again!!")
+    elif (computer_choice == beats[user_choice]):
+        print("Computer wins!!")
+    elif (user_choice == beats[computer_choice]):
+        print ("You win.")
+    else:
+        print("Error!")
+
+def play():
+    get_computer_choice()
+    get_user_choice()
+    get_winner
+
+
+
